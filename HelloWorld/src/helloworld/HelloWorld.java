38a74e88a61d4d534ebdd441cdd42213ce88c711
Mags <Mags@192.168.2.140>
2018-12-17 17:27:56 -0500

    Wrote the hello world program

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * A genertic Hello \world program
  */
 package helloworld;
 
@@ -12,6 +10,7 @@ package helloworld;
 public class HelloWorld {
 
     /**
+     * Prints "Hello World" to the console
      * @param args the command line arguments
      */
     public static void main(String[] args) {
